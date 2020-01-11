1b7d43b9335e5521caca40d6cafe7424b87d089d
notarthurphan <nahpruhtraton@gmail.com>
2020-01-11 00:11:55 -0800

    final changes for now

--- components/MessageList.js
+++ components/MessageList.js
@@ -19,7 +19,7 @@ class MessageList extends React.Component{
             <div className = "message-list">
                 {DUMMY_DATA.map((message, index) => {
                     return (
-                        <div className="message">
+                        <div key={index} className="message">
                             <div className="message-username">{message.senderId}</div>
                             <div className="message-text">{message.text}</div>
                         </div>
---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/777432fb-7020-4529-aa8d-d944028417f3/token";
+const instanceLocator = "v1:us1:777432fb-7020-4529-aa8d-d944028417f3";
+
+export{ tokenUrl, instanceLocator }
