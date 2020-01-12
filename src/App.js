a66a9bb3d81edb610aba74a8829de4f3a4c2345e
notarthurphan <nahpruhtraton@gmail.com>
2020-01-11 20:19:05 -0800

    chatroom working

--- src/App.js
+++ src/App.js
@@ -30,9 +30,9 @@ export default App;
 // App.js
 import React from 'react'
 import Chatkit from '@pusher/chatkit'
-import MessageList from '.components/MessageList'
-import SendMessageForm from '.components/SendMessageForm'
-import RoomList from '.components/RoomList'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
 import { tokenUrl, instanceLocator } from './config'
